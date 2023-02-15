ea1a29be161ddb850bb7740e925d917404300279
ruben-b4 <ruben.brichau@student.odisee.be>
2023-02-15 23:02:41 +0100

    ik geef het op

--- SlnLes01HerhalingAanvulling/ConsoleTafels/Program.cs
+++ SlnLes01HerhalingAanvulling/ConsoleTafels/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,51 +9,88 @@ namespace ConsoleTafels
 {
     internal class Program
     {
-        private static void Druktafel(int get = 4, int tel = 2)
-        {
-            int i;
-            
-            Console.WriteLine("4x8 tafel:");
-            for (i = 1; i <= 8; i++)
-            {
-                Console.Write("{0} x {1} = {2} \n", get, i, i * get);
-            }
+        // private static void Druktafel(int get = 4, int tel = 2)
+        // {
+        //    int i;
 
-            Console.WriteLine("\n2x5 tafel:");
-            for (i = 1; i <= 5; i++)
-            {
-                Console.Write("{0} x {1} = {2} \n", tel, i, i * tel);
-            }
-        }
+        // Console.WriteLine("4x8 tafel:");
+        //    for (i = 1; i <= 8; i++)
+        //    {
+        //        Console.Write("{0} x {1} = {2} \n", get, i, i * get);
+        //    }
 
-        private static void Deel2()
-        {
-            int get, len;
+        // Console.WriteLine("\n2x5 tafel:");
+        //    for (i = 1; i <= 5; i++)
+        //    {
+        //        Console.Write("{0} x {1} = {2} \n", tel, i, i * tel);
+        //    }
+        // }
 
-            Console.Write("\nGeef een getal: ");
-            get = Convert.ToInt32(Console.ReadLine());                     // onthoudt getal (get)
-            if (get <= 0)
-            {
-                Console.Write("Het getal moet positief zijn!");
-                Console.Write(" Geef een getal: ");
-                get = int.Parse(Console.ReadLine());
-            }
+        // private static int VraagPositiefGetal(string waarvoor)
+        // {
+        //    int get, len;
 
-            Console.Write("Geef een lengte: ");
-            len = int.Parse(Console.ReadLine());
+        // Console.Write("\nGeef een getal: ");
+        //    get = Convert.ToInt32(Console.ReadLine());                     // onthoudt getal (get)
+        //    if (get <= 0)
+        //    {
+        //        Console.Write("Het getal moet positief zijn!");
+        //        Console.Write(" Geef een getal: ");
+        //        get = int.Parse(Console.ReadLine());
+        //    }
 
-            Console.WriteLine("7x3 tafel:");
-            for (int i = 1; i <= len; i++)                      
-            {
-                Console.WriteLine(get + " x " + i + " = " + get * i);     // schrijft getal x i = getal*i, for telt I++ op tot het lengte haalt
-            }
-            Console.ReadKey();
-        }
+        // Console.Write("Geef een lengte: ");
+        //    len = int.Parse(Console.ReadLine());
 
+        // Console.WriteLine("7x3 tafel:");
+        //    for (int i = 1; i <= len; i++)
+        //    {
+        //        Console.WriteLine(get + " x " + i + " = " + get * i);     // schrijft getal x i = getal*i, for telt I++ op tot het lengte haalt
+        //    }
+        //    Console.ReadKey();
+        // }
+
+        // static void Main(string[] args)
+        // {
+        //    Druktafel();
+        //    VraagPositiefGetal();
+        // }
         static void Main(string[] args)
         {
-            Druktafel();
-            Deel2();
+            Console.WriteLine(Druktafel(4, 8));
+            Console.WriteLine(Druktafel(2, 5));
+
+            int getal = VraagpositiefGetal("Geef een getal: ");
+            int lengte = VraagpositiefGetal("Geef een lengte: ");
+
+            Console.WriteLine(Druktafel(getal, lengte));
+            Console.ReadLine();
+        }
+
+        static string Druktafel(int getal1, int getal2)
+        {
+            // tonen van maaltafel
+            string maal = $"{getal1}x{getal2} tafel:{Environment.NewLine}";
+
+            // berekening van maaltafel
+            for (int teller = 1; teller <= getal2; teller++)
+            {
+                maal += $"{getal1} x {teller} = {getal1 * teller}{Environment.NewLine}";
+            }
+            return maal;
+        }
+
+        static int VraagpositiefGetal(string vraag)
+        {
+            Console.Write(vraag);
+            int getal = Convert.ToInt32(Console.ReadLine());
+            while (getal < 1)
+            {
+                Console.Write("Het getal moet positief zijn! ");
+                Console.Write(vraag);
+                getal = Convert.ToInt32(Console.ReadLine());
+            }
+            return getal;
         }
     }
 }
--- SlnLes01HerhalingAanvulling/WpfTaken/MainWindow.xaml.cs
+++ SlnLes01HerhalingAanvulling/WpfTaken/MainWindow.xaml.cs
@@ -21,42 +21,120 @@ namespace WpfTaken
     /// </summary>
     public partial class MainWindow : Window
     {
+        static bool istoevoegengeklikt = false;
+        static Stack<ListBoxItem> verwijderdeitems = new Stack<ListBoxItem>();
+
         public MainWindow()
         {
             InitializeComponent();
+            CbbPrioriteit.SelectedIndex = 0;
+            tbkFoutmeldingen.Text = string.Empty;
         }
 
-        private void btnToevoegen_Click(object sender, RoutedEventArgs e)
+        private List<string> CheckForm()
         {
-           
+            List<string> errors = new List<string>();
+            if (!istoevoegengeklikt) return errors;
 
-            if (txtTaak.Text == "")
+            // reset
+            txtTaak.Text = string.Empty;
+            CbbPrioriteit.SelectedIndex = 0;
+            DateDeadline.SelectedDate = null;
+            RbdAdam.IsChecked = false;
+            RbdBilal.IsChecked = false;
+            RbnChelsey.IsChecked = false;
+            tbkFoutmeldingen.Text = string.Empty;
+            txtTaak.BorderBrush = Brushes.LightGray;
+            CbbPrioriteit.BorderBrush = Brushes.LightGray;
+            DateDeadline.BorderBrush = Brushes.LightGray;
+
+            // form controleren
+            if (txtTaak.Text == string.Empty)
             {
-                lblTaakfout.Text = "Gelieve een taak in te vullen";
+                errors.Add($"Gelieve een taak toe te voegen {Environment.NewLine}");
+                txtTaak.BorderBrush = Brushes.Red;
             }
 
-            if (CBoxPrio.SelectedIndex == -1)
+            if (CbbPrioriteit.SelectedIndex == 0)
             {
-                lblPrioriteit.Text = "Gelieve een prioriteit te kiezen";
+                errors.Add(tbkFoutmeldingen.Text += $"Gelieve een prioriteit toe te voegen {Environment.NewLine}");
+                CbbPrioriteit.BorderBrush = Brushes.Red;
             }
 
             if (DateDeadline.SelectedDate == null)
             {
-                lblDeadlinefout.Text = "Gelieve een deadline te kiezen";
+                errors.Add($"gelieve een deadline te kiezen {Environment.NewLine}");
+                DateDeadline.BorderBrush = Brushes.Red;
             }
 
-            if (rbtn1.IsChecked != true && rbtn2.IsChecked != true && rbtn3.IsChecked != true)
+            if (RbdAdam.IsChecked == false && RbdBilal.IsChecked == false && RbnChelsey.IsChecked == false)
             {
-               lblUitvoerderfout.Text = "Gelieve een uitvoerder te kiezen";
+                errors.Add($"Gelieve een uitvoerder te kiezen{Environment.NewLine}");
+                RbdAdam.BorderBrush = Brushes.Red;
+                RbnChelsey.BorderBrush = Brushes.Red;
+                RbdBilal.BorderBrush = Brushes.Red;
             }
 
+            return errors;
         }
 
-        private void btnVerwijderen_Click(object sender, RoutedEventArgs e)
+        private void BtnToevoegen_Click(object sender, RoutedEventArgs e)
         {
-            
-            lstItems.Items.RemoveAt
-            (lstItems.Items.IndexOf(lstItems.SelectedItem));
+            istoevoegengeklikt = true;
+            tbkFoutmeldingen.Text = string.Empty;
+            List<string> errors = CheckForm();
+            tbkFoutmeldingen.Text = string.Join(Environment.NewLine, errors);
+
+            if (errors.Count > 0) return;
+
+            // Voeg taak toe
+            ListBoxItem taak = new ListBoxItem();
+            string taakuitvoerder = string.Empty;
+            List<RadioButton> radioButtons = new List<RadioButton>() { RbdAdam, RbdBilal, RbnChelsey };
+            taakuitvoerder = GetCheckedButton(radioButtons).Content.ToString();
+            taak.Content = $"{txtTaak.Text} (Deadline: {DateDeadline.SelectedDate.Value.ToShortDateString()}; door: {taakuitvoerder})";
+            List<SolidColorBrush> colors = new List<SolidColorBrush>() { Brushes.IndianRed, Brushes.LightGreen, Brushes.Beige };
+            taak.Background = colors[CbbPrioriteit.SelectedIndex - 1];
+
+            lsbTaken.Items.Add(taak);
+        }
+
+        private RadioButton GetCheckedButton(List<RadioButton> radioButtons)
+        {
+            foreach (RadioButton radioButton in radioButtons)
+            {
+                if (radioButton.IsChecked == true) return radioButton;
+            }
+            return null;
+        }
+
+        private void SetButtonStates()
+        {
+            BtnTerugzetten.IsEnabled = verwijderdeitems.Count == 0 ? false : true;
+            BtnVerwijderen.IsEnabled = lsbTaken.SelectedIndex >= 0;
+        }
+
+        private void SelectionChanged(object sender, EventArgs e)
+        {
+            CheckForm();
+        }
+
+        private void LsbTaken_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            SetButtonStates();
+        }
+
+        private void BtnVerwijderen_Click(object sender, RoutedEventArgs e)
+        {
+            verwijderdeitems.Push((ListBoxItem)lsbTaken.SelectedItem);
+            lsbTaken.Items.Remove(lsbTaken.SelectedItem);
+            SetButtonStates();
+        }
+
+        private void BtnTerugzetten_Click(object sender, RoutedEventArgs e)
+        {
+            lsbTaken.Items.Add(verwijderdeitems.Pop());
+            SetButtonStates();
         }
     }
 }
