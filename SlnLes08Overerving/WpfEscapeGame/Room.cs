bb7ed58da640c0666b39c5f8dadf8145d8134bc0
ruben-b4 <ruben.brichau@student.odisee.be>
2023-05-10 09:30:29 +0200

    last escapegame

--- SlnLes08Overerving/WpfEscapeGame/Room.cs
+++ SlnLes08Overerving/WpfEscapeGame/Room.cs
@@ -10,12 +10,13 @@ namespace WpfEscapeGame
 {
     internal class Room : Actor
     {
+        // https://softwareengineering.stackexchange.com/questions/333591/is-it-ok-to-have-an-empty-abstract-class-to-make-concrete-classes-polymorphic
         public List<Item> Items { get; set; } = new List<Item>();
         public ImageSource Image { get; set; }
         public Room(string name, string desc, ImageSource image)
             : base(name, desc)
         {
-            Image = image;
+            this.Image = image;
         }
     }
 }
